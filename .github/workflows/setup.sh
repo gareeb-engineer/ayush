1f06bab065b1d05ca5bfbf34e53f1c82e707a928
ayushnigam0001 <ayushnigam0001@gmail.com>
2025-07-13 15:16:59 +0530

    added workflow

---
+++ .github/workflows/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd build-output
+pwd
+git init
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+git remote add origin https://github.com/gareeb-engineer/ayush.git
+touch .nojekyll
+git add .
+git commit -m "Deploy Antora site"
+git branch -M deploy
+#git push -f origin deploy
